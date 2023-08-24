9b698d57a8ec99ddc8c8d8ac31a6630fe9bfae86
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-24 23:24:52 +0530

    updated

---
+++ Caesar-Cipher/caesar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class caesar{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String message = input.nextLine();
+        
+        System.out.println(message));
+        
+    }
+}
