b7a0490c29334af1eabfdd3b929dd62cca65fe14
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-24 23:36:17 +0530

    caesar

--- Caesar-Cipher/caesar.java
+++ Caesar-Cipher/caesar.java
@@ -1,11 +1,38 @@
 import java.util.Scanner;
 
-class caesar{
+public class caesar {
+
+    public static String encrypt(String plaintext, int shift) {
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (char c : plaintext.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                encryptedText.append((char) ((c - base + shift) % 26 + base));
+            } else {
+                encryptedText.append(c);
+            }
+        }
+
+        return encryptedText.toString();
+    }
+
+    public static String decrypt(String ciphertext, int shift) {
+        return encrypt(ciphertext, 26 - shift);
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String message = input.nextLine();
-        
-        System.out.println(message));
-        
+        Scanner ip = new Scanner(System.in); 
+        System.out.println("Enter the Message to encrypt: ");
+        String message = ip.nextLine();
+        System.out.println("Enter the no of shifts needed :");
+        int shift = ip.nextInt();
+
+        String encryptedMessage = encrypt(message, shift);
+        String decryptedMessage = decrypt(encryptedMessage, shift);
+
+        System.out.println("Original Message: " + message);
+        System.out.println("Encrypted Message: " + encryptedMessage);
+        System.out.println("Decrypted Message: " + decryptedMessage);
     }
 }
