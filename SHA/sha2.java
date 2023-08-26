be89ac7cb8ee51920d1dbe718608e8d503768b5b
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-26 19:49:43 +0530

    Sha 256

---
+++ SHA/sha2.java
@@ -0,0 +1,38 @@
+package SHA;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+
+public class sha2{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the message you need to hash :");
+        String Message = input.nextLine();
+    
+        System.out.println("And the SHA-256 hash is"+SHA256(Message));
+    }
+
+    static String SHA256(String input){
+        try{
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes());
+            
+            StringBuilder hexHash = new StringBuilder();
+            for(byte h: hash){
+                hexHash.append(String.format("%02x", h));
+            }
+
+            return hexHash.toString();
+        }
+        catch(NoSuchAlgorithmException ex){
+            ex.printStackTrace();
+            return null;
+
+        }
+
+    }
+
+}
