50ce526096bfb438c2c7d32c74f9681205e7cf4a
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-28 23:27:39 +0530

    ecc

---
+++ ECC/ecc.java
@@ -0,0 +1,28 @@
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Security;
+
+public class ecc {
+    public static void main(String[] args) throws Exception {
+        // Add Bouncy Castle as a security provider
+        Security.addProvider(new BouncyCastleProvider());
+
+        // Choose an elliptic curve (e.g., secp256k1)
+        ECNamedCurveTable ecParams = ECNamedCurveTable.getParameterSpec("secp256k1");
+
+        // Generate an ECC key pair
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
+        keyPairGenerator.initialize(ecParams);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        // Get the public and private keys
+        java.security.interfaces.ECPublicKey publicKey = (java.security.interfaces.ECPublicKey) keyPair.getPublic();
+        java.security.interfaces.ECPrivateKey privateKey = (java.security.interfaces.ECPrivateKey) keyPair.getPrivate();
+
+        System.out.println("Public Key: " + publicKey);
+        System.out.println("Private Key: " + privateKey);
+    }
+}
